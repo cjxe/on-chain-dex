73850ee35bd431291d0bbe6994952656aad02d9f
Baran <scgc47@durham.ac.uk>
2022-02-21 21:05:06 +0000

    save sell/buy order IDs in bytes32[]

--- backend/contracts/Exchange.sol
+++ backend/contracts/Exchange.sol
@@ -2,41 +2,35 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-// import "contracts/USDb.sol"; // instead of importing the contract, import the interface
 import "contracts/IUSDb.sol";
 import {LinkedListLib} from "contracts/LinkedList.sol";
 
 contract Exchange is Ownable {
+    
     // STORAGE
     address private tokenA = 0xd9145CCE52D386f254917e481eB44e9943F39138;
     address private tokenB = 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8;
-    mapping(address => uint256) reserves;
-    mapping(address => mapping(address => uint256)) deposits; // addr A deposit B token, C many
-    mapping(address => mapping(uint256 => uint256)) sellOrders; // addr A sell for B, C many
-    mapping(address => mapping(uint256 => uint256)) buyOrders; // addr A buy for B, C many
+    mapping (address => uint256) reserves;
+    mapping (address => mapping (address => uint256)) deposits; // addr A deposit B token, C many
+    mapping (address => bytes32[]) sellOrders; // addr A: [sellOrderIds]
+    mapping (address => bytes32[]) private _buyOrders; // addr A: [buyOrderIds]
 
-    mapping(address => mapping(uint256 => LinkedListLib.LinkedList)) orderBook; // token A, price B, orders[seller, amount]
+    mapping (address => mapping (uint256 => LinkedListLib.LinkedList)) orderBook; // token A, price B, orders[seller, amount]
 
-    function deposit(address tokenAddress, uint256 amount)
-        private
-        returns (bool)
-    {
+    function deposit (address tokenAddress, uint256 amount) private returns (bool) {
         // TODO:
         // - [ ] introduce how much of the pool you own
         // - [ ] make it internal
 
-        require(
-            tokenAddress == tokenA || tokenAddress == tokenB,
-            "Deposited token is not in the pool."
-        );
-
+        require(tokenAddress == tokenA || tokenAddress == tokenB, "Deposited token is not in the pool.");
+        
         // subtract token amount from depositer => this contract needs to get approved to spend
         // 1- approve that this contract can use invoker's tokenA/B
         // approve(address(this), amount); // this doesn't work because right now (with contract Exchange is USDb{...}) we
-        // are making the Exchange contract have all the functions of USDb (making it USDb + anything in the Exchange contract).
+        // are making the Exchange contract have all the functions of USDb (making it USDb + anything in the Exchange contract). 
         // In order to access the contract (without adding it to the current contract), we need some sort of API => interface of USDb.
         // IUSDb(tokenAddress).approve(address(this), 5); // caller is `this` contract, so this line is approving to itself.
-        // Thus, approving must be done by calling the original USDb by us to say `this` contract can spend our USDb
+        // Thus, approving must be done by calling the original USDb by us to say `this` contract can spend our USDb 
         //
         // 2- call `transferFrom()`
         IUSDb(tokenAddress).transferFrom(msg.sender, address(this), amount);
@@ -45,42 +39,18 @@ contract Exchange is Ownable {
         return true;
     }
 
-    function withdraw(address tokenAddress, uint256 amount)
-        private
-        returns (bool)
-    {
-        require(
-            tokenAddress == tokenA || tokenAddress == tokenB,
-            "Withdrawn token is not in the pool."
-        );
-        require(
-            deposits[msg.sender][tokenAddress] >= amount,
-            "Withdraw amount exceeds deposited."
-        );
+    function withdraw (address tokenAddress, uint256 amount) private returns (bool) {
+        require(tokenAddress == tokenA || tokenAddress == tokenB, "Withdrawn token is not in the pool.");
+        require(deposits[msg.sender][tokenAddress] >= amount, "Withdraw amount exceeds deposited.");
         IUSDb(tokenAddress).transfer(msg.sender, amount);
         deposits[msg.sender][tokenAddress] -= amount;
         reserves[tokenAddress] -= amount;
         return true;
     }
 
-    function getDeposits(address account, address tokenAddress)
-        public
-        view
-        returns (uint256)
-    {
-        require(
-            tokenAddress == tokenA || tokenAddress == tokenB,
-            "Token is not in the pool."
-        );
+    function getDeposits(address account, address tokenAddress) public view returns (uint256) {
+        require(tokenAddress == tokenA || tokenAddress == tokenB, "Token is not in the pool.");
         return deposits[account][tokenAddress];
-    }
-
-    function getPrice() public view returns (uint256) {
-        require(
-            reserves[tokenA] != 0 && reserves[tokenB] != 0,
-            "One token does not exist in the pool."
-        );
-        return (reserves[tokenA] / reserves[tokenB]);
     }
 
     // Sell
@@ -88,19 +58,14 @@ contract Exchange is Ownable {
         deposit(tokenA, amount);
         // TODO
         // check if buy order matches the price
-        sellOrders[msg.sender][price] += amount;
-
+        
         LinkedListLib.Order memory o;
         o.seller = msg.sender;
         o.amount = amount;
 
         // if no node, initHead
         if (orderBook[tokenA][price].length == 0) {
-            LinkedListLib.initHead(
-                orderBook[tokenA][price],
-                o.seller,
-                o.amount
-            );
+            LinkedListLib.initHead(orderBook[tokenA][price], o.seller, o.amount);
         } else {
             LinkedListLib.addNode(orderBook[tokenA][price], o.seller, o.amount);
         }
@@ -108,32 +73,21 @@ contract Exchange is Ownable {
         return true;
     }
 
-    function getSellOrders(uint256 price)
-        public
-        view
-        returns (LinkedListLib.Order[] memory)
-    {
-        LinkedListLib.Order[] memory orders = new LinkedListLib.Order[](
-            orderBook[tokenA][price].length
-        );
-
+    function getAllSellOrders(uint256 price) public view returns(LinkedListLib.Order[] memory) {
+        LinkedListLib.Order[] memory orders = new LinkedListLib.Order[](orderBook[tokenA][price].length);
+        
         bytes32 currId = orderBook[tokenA][price].head;
 
-        for (uint256 i = 0; i < orderBook[tokenA][price].length; i++) {
-            orders[i] = orderBook[tokenA][price].nodes[currId].order;
-            currId = orderBook[tokenA][price].nodes[currId].next;
+        for (uint256 i=0; i < orderBook[tokenA][price].length; i++) {
+            orders[i] = orderBook[tokenA][price].nodes[currId].order; 
+            currId = orderBook[tokenA][price].nodes[currId].next;  
         }
         return orders;
     }
 
-    function activeSellOrders(uint256 price) public view returns (uint256) {
-        return sellOrders[msg.sender][price];
-    }
-
     // broken
-    function deleteSellOrder(uint256 price) public returns (bool) {
+    function deleteSellOrder() public returns (bool) {
         withdraw(tokenA, deposits[msg.sender][tokenA]);
-        sellOrders[msg.sender][price] = 0;
 
         // TODO
         // - [ ] del from `orderBook`
@@ -142,77 +96,78 @@ contract Exchange is Ownable {
 
     // Buy
     function newBuyOrder(uint256 price, uint256 amount) public returns (bool) {
-        deposit(tokenB, price * amount);
-        buyOrders[msg.sender][price] += price * amount; // - [ ] make edits on this
+        deposit(tokenB, price*amount);
 
         uint256 len = orderBook[tokenA][price].length;
-        for (uint8 i = 0; i < len; i++) {
+        for (uint8 i=0; i < len; i++) {
             bytes32 head_ = orderBook[tokenA][price].head;
-            uint256 sellAmount = orderBook[tokenA][price]
-                .nodes[head_]
-                .order
-                .amount;
+            uint256 sellAmount = orderBook[tokenA][price].nodes[head_].order.amount;
 
             if (amount == 0) {
                 return true;
+
             } else if (sellAmount <= amount) {
                 // buy amount >= sell amount
-                LinkedListLib.Order memory o = orderBook[tokenA][price]
-                    .nodes[head_]
-                    .order;
+                LinkedListLib.Order memory o = orderBook[tokenA][price].nodes[head_].order;
                 LinkedListLib.popHead(orderBook[tokenA][price]);
 
                 reserves[tokenA] -= o.amount;
                 deposits[o.seller][tokenA] -= o.amount;
                 IUSDb(tokenA).transfer(msg.sender, o.amount);
-                IUSDb(tokenB).transfer(o.seller, price * o.amount);
+                IUSDb(tokenB).transfer(o.seller, price*o.amount);
                 amount -= o.amount;
+
             } else if (sellAmount > amount) {
-                LinkedListLib.Order memory o = orderBook[tokenA][price]
-                    .nodes[head_]
-                    .order;
+                LinkedListLib.Order memory o = orderBook[tokenA][price].nodes[head_].order;
                 orderBook[tokenA][price].nodes[head_].order.amount -= amount;
 
                 reserves[tokenA] -= amount;
                 deposits[o.seller][tokenA] -= amount;
                 IUSDb(tokenA).transfer(msg.sender, amount);
-                IUSDb(tokenB).transfer(o.seller, price * amount);
+                IUSDb(tokenB).transfer(o.seller, price*amount);
                 amount = 0;
+
             }
         }
         // new buy order
         if (orderBook[tokenB][price].length == 0) {
-            LinkedListLib.initHead(
-                orderBook[tokenB][price],
-                msg.sender,
-                amount
-            );
+            bytes32 orderId = LinkedListLib.initHead(orderBook[tokenB][price], msg.sender, price*amount); // [ ] test this
+            _buyOrders[msg.sender].push(orderId);
         } else {
-            LinkedListLib.addNode(orderBook[tokenB][price], msg.sender, amount);
+            bytes32 orderId = LinkedListLib.addNode(orderBook[tokenB][price], msg.sender, price*amount);
+            _buyOrders[msg.sender].push(orderId);
         }
 
         return true;
     }
 
-    function getBuyOrders(uint256 price)
-        public
-        view
-        returns (LinkedListLib.Order[] memory)
-    {
-        LinkedListLib.Order[] memory orders = new LinkedListLib.Order[](
-            orderBook[tokenB][price].length
-        );
+    function deleteBuyOrder(uint256 price, bytes32 id) public returns (bool) {
+        LinkedListLib.Node memory n = LinkedListLib.getNode(orderBook[tokenB][price], id);
+        require(LinkedListLib.deleteNode(orderBook[tokenB][price], id) == true);
+        //_buyOrders
+        withdraw(tokenB, n.order.amount);
+        return true;
+    }
 
+    function getAllBuyOrders(uint256 price) public view returns(LinkedListLib.Order[] memory) {
+        LinkedListLib.Order[] memory orders = new LinkedListLib.Order[](orderBook[tokenB][price].length);
+        
         bytes32 currId = orderBook[tokenB][price].head;
 
-        for (uint256 i = 0; i < orderBook[tokenB][price].length; i++) {
-            orders[i] = orderBook[tokenB][price].nodes[currId].order;
-            currId = orderBook[tokenB][price].nodes[currId].next;
+        for (uint256 i=0; i < orderBook[tokenB][price].length; i++) {
+            orders[i] = orderBook[tokenB][price].nodes[currId].order; 
+            currId = orderBook[tokenB][price].nodes[currId].next;  
         }
         return orders;
     }
+    
+    function activeBuyOrders() public view returns (bytes32[] memory) {
+        bytes32[] memory buyOrders = new bytes32[](_buyOrders[msg.sender].length);
 
-    function activeSellOrders(uint256 price) public view returns (uint256) {
-        return sellOrders[msg.sender][price];
+        for (uint256 i=0; i < _buyOrders[msg.sender].length; i++) {
+            buyOrders[i] = _buyOrders[msg.sender][i]; 
+        }
+        return buyOrders;
     }
+
 }
